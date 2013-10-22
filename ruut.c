e19a891e0b2a8bf4fa4d5f5bbcf425923e0d0ced
mattis5646 <mattis.kivilatte.005@mail.ee>
2013-10-22 16:43:55 +0300

    3.kodus

---
+++ ruut.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(int argc, char* argv [])
+{
+	printf("\narv\truut\n");    
+	int i,j;		    
+	for(i=1;i<=20;i++)          
+	{
+		printf("%2d\t%3d\n",i,j=i*i);  
+	}	
+	return 0;
+}
